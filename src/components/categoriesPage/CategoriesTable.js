8131cb1584cef337f597057a8bdfb727a0a17b7d
nickmazurenko <surprisedsunrise@gmail.com>
2022-10-29 14:35:49 +0300

    add search params to categories

--- src/components/categoriesPage/CategoriesTable.js
+++ src/components/categoriesPage/CategoriesTable.js
@@ -1,6 +1,7 @@
 /* eslint-disable operator-linebreak */
 import { Pagination } from 'flowbite-react';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import CategoryCard from './CategoryCard';
 import CategoriesTableHeader from './CategoriesTableHeader';
 import config from '../../config';
@@ -16,10 +17,13 @@ const slicePages = (array, currentPage) => {
 };
 
 const CategoriesTable = ({ categories }) => {
-  const [sort, setSort] = useState({ param: 'postsCount', ascending: false });
+  const [params, setParams] = useSearchParams();
   const [currentPage, setCurrentPage] = useState(1);
   const [allCategories, setAllCategories] = useState(
-    arraySort(categories, sort)
+    arraySort(categories, {
+      param: params.get('param'),
+      ascending: params.get('ascending') === 'true',
+    })
   );
   const [currentCategories, setCurrentCategories] = useState(
     slicePages(allCategories, currentPage)
@@ -30,16 +34,31 @@ const CategoriesTable = ({ categories }) => {
     setCurrentCategories(slicePages(allCategories, page));
   };
 
+  const start = () => {
+    const search = params.get('search');
+    setParams({ param: 'postsCount', ascending: false });
+
+    if (search) {
+      setAllCategories(
+        allCategories.filter((category) => {
+          return (
+            category.title.toLowerCase().includes(search.toLowerCase()) ||
+            category.description.toLowerCase().includes(search.toLowerCase())
+          );
+        })
+      );
+    }
+  };
+
   useEffect(() => {
     loadCategories(currentPage);
+    start();
   }, [allCategories]);
 
   return (
     <>
       <div className='h-full w-full'>
         <CategoriesTableHeader
-          sort={sort}
-          setSort={setSort}
           categories={categories}
           allCategories={allCategories}
           setAllCategories={setAllCategories}
--- src/components/categoriesPage/CategoriesTableHeader.js
+++ src/components/categoriesPage/CategoriesTableHeader.js
@@ -5,15 +5,15 @@ import {
   HiSearch,
   HiDocumentText,
 } from 'react-icons/hi';
+import { useSearchParams } from 'react-router-dom';
 import { arraySort } from '../../utils/categories.utils';
 
 const CategoriesTableHeader = ({
-  sort,
-  setSort,
   categories,
   allCategories,
   setAllCategories,
 }) => {
+  const [params, setSearchParams] = useSearchParams();
   const startSearch = (search) => {
     setAllCategories(
       allCategories.filter((category) => {
@@ -26,18 +26,26 @@ const CategoriesTableHeader = ({
   };
 
   const onSearch = (event) => {
-    if (event.target.value.length) startSearch(event.target.value);
-    else {
+    if (event.target.value.length) {
+      startSearch(event.target.value);
+      params.set('search', event.target.value);
+      setSearchParams(params);
+    } else {
       setAllCategories(categories);
+      params.set('search', '');
+      setSearchParams(params);
     }
   };
 
   const sortBy = (param) => {
     const newSort = {
       param,
-      ascending: sort.param === param ? !sort.ascending : false,
+      ascending:
+        params.get('param') === param
+          ? !(params.get('ascending') === 'true')
+          : false,
     };
-    setSort(newSort);
+    setSearchParams(newSort);
     const sorted = arraySort(allCategories, newSort);
     setAllCategories(sorted);
   };
@@ -45,17 +53,28 @@ const CategoriesTableHeader = ({
   return (
     <div className='md:flex justify-between items-center p-4 border-5 border-white rounded-b-lg bg-gray-900'>
       <span className='text-3xl font-bold text-gray-500'>All Categories</span>
-      <Dropdown size='sm' arrowIcon={false}
+      <Dropdown
+        size='sm'
+        arrowIcon={false}
         label={
           <>
             <span className='mr-4'>
-              {sort.param.charAt(0).toUpperCase() + sort.param.slice(1)}
+              {params.get('param')
+                ? params.get('param').charAt(0).toUpperCase()
+                  + params.get('param').slice(1)
+                : ''}
             </span>
-            {sort.ascending ? (<HiSortAscending size={25} />) : (<HiSortDescending size={25} />)}
+            {params.get('ascending') === 'true' ? (
+              <HiSortAscending size={25} />
+            ) : (
+              <HiSortDescending size={25} />
+            )}
           </>
         }>
         <Dropdown.Item
-          onClick={() => { sortBy('postsCount'); }}
+          onClick={() => {
+            sortBy('postsCount');
+          }}
           icon={HiDocumentText}>
           Post Count
         </Dropdown.Item>
@@ -71,6 +90,7 @@ const CategoriesTableHeader = ({
           <input
             type='text'
             onChange={onSearch}
+            value={params.get('search')}
             id='table-search-categories'
             className='block p-2 pl-10 w-60 text-sm rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500'
             placeholder='Search for categories'
